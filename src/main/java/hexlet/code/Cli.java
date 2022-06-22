e456dfbc3c862c574aa258d3f07e7bc751e66f66
evgeniy1503 <evgeniy15031991@mail.ru>
2022-06-22 15:36:39 +0300

    add checkstyle, change README.md

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Cli {
     public static void playerName() {
         System.out.print("May I have your name? ");
+
         Scanner in = new Scanner(System.in);
         String name = in.nextLine();
         System.out.println("Hello, " + name + "!");
